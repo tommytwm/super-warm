f331c20a15b7a23180fabda9def3fba4db6762ca
Tommy Tho <tommythowm@gmail.com>
2019-11-30 11:26:27 -0800

    Updates

---
+++ precache-manifest.78d030898edd81efd24a6647bd90b22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ce1bdcdc8a085bed8ea85fe01e07b4",
+    "url": "/super-warm/index.html"
+  },
+  {
+    "revision": "b35367eea1b3c4bfc2c3",
+    "url": "/super-warm/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b2650f473ab5af92f5e7",
+    "url": "/super-warm/static/js/2.6f5e968f.chunk.js"
+  },
+  {
+    "revision": "b35367eea1b3c4bfc2c3",
+    "url": "/super-warm/static/js/main.03ec881b.chunk.js"
+  },
+  {
+    "revision": "8bb0cb24e8d04f5a8d53",
+    "url": "/super-warm/static/js/runtime-main.627dbd4f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/super-warm/static/media/logo.25bf045c.svg"
+  }
+]);
