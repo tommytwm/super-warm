05d0256ee765bdc014dcfcbc7f85d0d8828a3db2
Tommy Tho <tommythowm@gmail.com>
2019-11-30 19:24:34 -0800

    demo

--- src/components/homePage/needToTalk.js
+++ src/components/homePage/needToTalk.js
@@ -1,7 +1,27 @@
-import React, {Fragment} from 'react'
-import { Segment, Button, Header, Menu, Label, Icon, Message } from 'semantic-ui-react'
+import React, { Fragment, Component } from 'react';
+import { 
+    Segment, 
+    Button, 
+    Header, 
+    Menu, 
+    Label, 
+    Icon, 
+    Message,
+    Modal,
+} from 'semantic-ui-react';
+import Demo from './demo';
 
-const NeedToTalk = () => (
+class NeedToTalk extends Component {
+  state = { open: false, isOpen: true }
+
+  show = (dimmer) => () => this.setState({ dimmer, open: true })
+  close = () => this.setState({ open: false })
+  isOpen = () => this.setState({ open: true })
+
+  render() {
+    const { open, dimmer } = this.state
+
+    return (
     <div>
         <Fragment>
         <Segment attached>
@@ -14,7 +34,12 @@ const NeedToTalk = () => (
         </Message>
         <Menu compact>
             <Menu.Item as='a'>
-                <Button><Icon name='user' />Talk to peer</Button>
+                <Button 
+                    onClick={this.show('inverted')}
+                    >
+                    <Icon name='user'/>
+                    Talk to a peer
+                </Button>
                 <Label color='teal' floating>
                     5
                 </Label>
@@ -27,9 +52,22 @@ const NeedToTalk = () => (
             </Menu.Item>
         </Menu>
 
+        <Modal dimmer={dimmer} open={open} onClose={this.close}>
+            <Demo/>
+          <Modal.Actions>
+            <Button
+                icon='close'
+                labelPosition='right'
+                onClick={this.close}
+                />
+          </Modal.Actions>
+        </Modal>
+
         </Segment>
         </Fragment>
     </div>
-)
+        )
+    }
+}
 
 export default NeedToTalk
